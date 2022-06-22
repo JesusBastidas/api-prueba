2930b8e84315ebeaa79d09f61846ee6dc92daf3e
JesusBastidas <davidbastidascarvajal@gmail.com>
2022-06-21 23:23:40 -0500

    cors

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Acceso a todos los origenes CORS
+app.use(cors());
+
+
 const PORT = process.env.PORT || 3050;
 const app = express();
 app.use(bodyParser.json())
