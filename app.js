0cd9f70b7cad305f32e88acb0c059b700ab14b3e
JesusBastidas <davidbastidascarvajal@gmail.com>
2022-06-22 11:48:45 -0500

    creados querys para insumos

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 
 //Ruta 
 app.get('/', (req, res) => {
-    res.send('Bienvenido a casa');
+    res.send('Bienvenido a la API de Salchichon Gonzalez');
 });
 
 //Todos los productos
@@ -62,7 +62,7 @@ app.get('/products/:id', (req, res) => {
 });
 
 // Nuevo producto
-app.post('/add', (req, res) => {
+app.post('/addproduct', (req, res) => {
     const sql = 'INSERT INTO products SET ?';
     const product = {
         name: req.body.name,
@@ -77,7 +77,7 @@ app.post('/add', (req, res) => {
 });
 
 // Actualizar producto
-app.put('/update/:id', (req, res) => {
+app.put('/updateproduct/:id', (req, res) => {
     const {id} = req.params;
     const {name, quantity, unitWeight, totalWeight} = req.body;
     const sql = `UPDATE products SET name = '${name}', quantity = ${quantity}, unitWeight = ${unitWeight}, totalWeight = ${totalWeight} WHERE id = ${id}`;
@@ -88,11 +88,76 @@ app.put('/update/:id', (req, res) => {
 });
 
 //Eliminar producto
-app.delete('/delete/:id', (req, res) => {
+app.delete('/deleteproduct/:id', (req, res) => {
     const {id} = req.params;
     const sql = `DELETE FROM products WHERE is = ${id}`;
     connection.query(sql, error =>{
         if (error) throw error;
         res.send('Producto eliminado');
+    });
+});
+
+//Todos los insumos
+app.get('/supplies', (req, res) => {
+    const sql = 'SELECT * FROM supplies';
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('No hay Insumos');
+        }
+    })
+});
+
+//Insumo por id
+app.get('/supplies/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const sql = `SELECT * FROM supplies WHERE id = ${id}`;
+    connection.query(sql, (error, result) => {
+        if (error) throw error;
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.send('No se encontro el insumo');
+        }
+    })
+});
+
+//Nuevo insumo
+app.post('/addsupply', (req, res) => {
+    const sql = 'INSERT INTO supplies SET ?';
+    const product = {
+        name: req.body.name,
+        quantity: req.body.quantity,
+        unitWeight: req.body.unitWeight,
+        totalWeight: req.body.totalWeight,
+    }
+    connection.query(sql, product, error => {
+        if (error) throw error;
+        res.send('Insumo agregado correctamente');
+    });
+});
+
+//Actualizar insumo
+app.put('/updatesupply/:id', (req, res) => {
+    const {id} = req.params;
+    const {name, quantity, unitWeight, totalWeight} = req.body;
+    const sql = `UPDATE supplies SET name = '${name}', quantity = ${quantity}, unitWeight = ${unitWeight}, totalWeight = ${totalWeight} WHERE id = ${id}`;
+    connection.query(sql, error => {
+        if (error) throw error;
+        res.send('Insumo actualizado correctamente');
+    });
+});
+
+//Eliminar Insumo
+app.delete('/deletesupply/:id', (req, res) => {
+    const {id} = req.params;
+    const sql = `DELETE FROM supplies WHERE is = ${id}`;
+    connection.query(sql, error =>{
+        if (error) throw error;
+        res.send('Insumo eliminado');
     });
 });
